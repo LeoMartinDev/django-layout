c4b80e71902879f69a2f7bc62a61559074c82f7c
Martin Mahner <martin@mahner.org>
2015-03-04 09:30:08 +0100

    Revert "Bump version"
    This is not the version of django-layout, it's the initial version
    of the project to be created. This reverts commit
    2195fe66837e547b0172f186be76317b4be25b1b.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='{{ project_name }}',
-    version='1.1',
+    version='1.0',
     description="",
     author="Lincoln Loop",
     author_email='info@lincolnloop.com',
