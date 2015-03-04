4ee16e9de1bdb28bcdb752c5117b1a13c6e66951
Martin Mahner <martin@mahner.org>
2015-03-04 09:42:18 +0100

    Removed old `patterns()` method. Fixes pull request #8.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,17 +1,15 @@
-from django.conf.urls.static import static
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include, static
 from django.conf import settings
-
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-   # (r'', include('{{ project_name }}.apps.')),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    # (r'', include('{{ project_name }}.apps.')),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
 
 if settings.DEBUG and settings.MEDIA_ROOT:
-    urlpatterns += static(settings.MEDIA_URL,
+    urlpatterns += static.static(settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT)
